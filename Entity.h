c80b7ebb47372df22c5b0bf29d9c30b0709369d8
Rachit Khandelwal <rachit7645@gmail.com>
2022-03-27 14:10:27 +0530

    Basic Entity System

---
+++ Entity.h
@@ -0,0 +1,22 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <glm/glm.hpp>
+
+#include "Util.h"
+#include "Model.h"
+
+namespace Entities
+{
+	class Entity
+	{
+	public:
+		Entity(const Renderer::Model &p_model, const glm::vec3 &pos, const glm::vec3 &rot, f32 sc);
+
+		Renderer::Model model;
+		glm::vec3 position, rotation;
+		f32 scale;
+	};
+}
+
+#endif
