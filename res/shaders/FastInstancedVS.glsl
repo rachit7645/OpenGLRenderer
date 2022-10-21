9afb118195f73da222c5788682f5a8794a9e3b4e
Rachit Khandelwal <rachit7645@gmail.com>
2022-10-21 20:39:57 +0530

    Improve fast instanced shaders

--- res/shaders/FastInstancedVS.glsl
+++ res/shaders/FastInstancedVS.glsl
@@ -1,5 +1,17 @@
 #version 430 core
 
+const float AMBIENT_STRENGTH = 1.0f;
+const int   MAX_LIGHTS       = 4;
+
+struct Light
+{
+	vec4 position;
+	vec4 ambient;
+	vec4 diffuse;
+	vec4 specular;
+	vec4 attenuation;
+};
+
 struct Instance
 {
 	mat4 modelMatrix;
@@ -9,6 +21,11 @@ layout(std140, binding = 0) uniform Matrices
 {
 	mat4 projectionMatrix;
 	mat4 viewMatrix;
+};
+
+layout(std140, binding = 1) uniform Lights
+{
+	Light lights[MAX_LIGHTS];
 };
 
 layout(std140, binding = 2) uniform Shared
@@ -28,13 +45,38 @@ layout(location = 0) in vec3 position;
 layout(location = 1) in vec2 textureCoords;
 layout(location = 2) in vec3 normal;
 
-out vec2 txCoords;
-out vec4 worldPosition;
+uniform sampler2D diffuseTexture;
+
+out vec4 vertexColor;
+
+float CalculateAttFactor(int index, vec4 worldPosition);
+vec4  CalculateAmbient(int index);
 
 void main()
 {
-	worldPosition      = instances[gl_InstanceID].modelMatrix * vec4(position, 1.0f);
+	vec4 worldPosition = instances[gl_InstanceID].modelMatrix * vec4(position, 1.0f);
 	gl_Position        = projectionMatrix * viewMatrix * worldPosition;
 	gl_ClipDistance[0] = dot(worldPosition, clipPlane);
-	txCoords           = textureCoords;
+
+	// Diffuse texture color
+	vec4 diffuseColor = texture(diffuseTexture,  textureCoords);
+	// Lighting loop
+	for (int i = 0; i < MAX_LIGHTS; ++i)
+	{
+		// Compute ambient
+		vertexColor += CalculateAmbient(i) * diffuseColor * CalculateAttFactor(i, worldPosition);
+	}
+}
+
+float CalculateAttFactor(int index, vec4 worldPosition)
+{
+	vec4  ATT       = lights[index].attenuation;
+	float distance  = length(lights[index].position.xyz - worldPosition.xyz);
+	float attFactor = ATT.x + (ATT.y * distance) + (ATT.z * distance * distance);
+	return 1.0f / attFactor;
+}
+
+vec4 CalculateAmbient(int index)
+{
+	return vec4(AMBIENT_STRENGTH * lights[index].ambient.rgb, 1.0f);
 }
