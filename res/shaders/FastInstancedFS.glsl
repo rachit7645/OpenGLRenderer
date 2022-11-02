b4b9479313a72f7b14903f12d5005ed396a5eb0e
Rachit Khandelwal <rachit7645@gmail.com>
2022-11-02 15:33:53 +0530

    Fix Fast Instanced Shader

--- res/shaders/FastInstancedFS.glsl
+++ res/shaders/FastInstancedFS.glsl
@@ -1,13 +1,68 @@
 #version 430 core
 
+const float AMBIENT_STRENGTH = 0.6f;
+const int   MAX_LIGHTS       = 4;
+
+struct Light
+{
+	vec4 position;
+	vec4 ambient;
+	vec4 diffuse;
+	vec4 specular;
+	vec4 attenuation;
+};
+
+layout(std140, binding = 1) uniform Lights
+{
+	Light lights[MAX_LIGHTS];
+};
+
 in vec2 txCoords;
-in vec4 vertexColor;
+in vec3 unitNormal;
+in vec3 unitLightVector[MAX_LIGHTS];
+in vec4 worldPosition;
 
 uniform sampler2D diffuseTexture;
 
 out vec4 outColor;
 
+float CalculateAttFactor(int index);
+vec4  CalculateAmbient(int index);
+vec4  CalculateDiffuse(int index);
+
 void main()
 {
-	outColor = vertexColor * texture(diffuseTexture, txCoords);
+	vec4 diffColor = texture(diffuseTexture, txCoords);
+
+	vec4 ambient = vec4(0.0f);
+	vec4 diffuse = vec4(0.0f);
+
+	for (int i = 0; i < MAX_LIGHTS; ++i)
+	{
+		float attFactor = CalculateAttFactor(i);
+		ambient        += CalculateAmbient(i) * diffColor * attFactor;
+		diffuse        += CalculateDiffuse(i) * diffColor * attFactor;
+	}
+
+	outColor = ambient + diffuse;
+}
+
+float CalculateAttFactor(int index)
+{
+	vec4  ATT       = lights[index].attenuation;
+	float distance  = length(lights[index].position.xyz - worldPosition.xyz);
+	float attFactor = ATT.x + (ATT.y * distance) + (ATT.z * distance * distance);
+	return 1.0f / attFactor;
+}
+
+vec4 CalculateAmbient(int index)
+{
+	return vec4(AMBIENT_STRENGTH * lights[index].ambient.rgb, 1.0f);
+}
+
+vec4 CalculateDiffuse(int index)
+{
+	float nDot1      = dot(unitNormal, unitLightVector[index]);
+	float brightness = max(nDot1, 0.0f);
+	return vec4(brightness * lights[index].diffuse.rgb, 1.0f);
 }
--- res/shaders/FastInstancedVS.glsl
+++ res/shaders/FastInstancedVS.glsl
@@ -1,8 +1,6 @@
 #version 430 core
 
-const float AMBIENT_STRENGTH = 1.0f;
-const int   MAX_LIGHTS       = 4;
-const int   NUM_LIGHTS       = 4;
+const int MAX_LIGHTS = 4;
 
 struct Light
 {
@@ -47,48 +45,22 @@ layout(location = 1) in vec2 textureCoords;
 layout(location = 2) in vec3 normal;
 
 out vec2 txCoords;
-out vec4 vertexColor;
-
-float CalculateAttFactor(int index, vec4 worldPosition);
-vec4  CalculateAmbient(int index);
-vec4  CalculateDiffuse(int index, vec3 normal, vec3 lightDir);
+out vec3 unitNormal;
+out vec3 unitLightVector[MAX_LIGHTS];
+out vec4 worldPosition;
 
 void main()
 {
-	vec4 worldPosition = instances[gl_InstanceID].modelMatrix * vec4(position, 1.0f);
+	worldPosition      = instances[gl_InstanceID].modelMatrix * vec4(position, 1.0f);
 	gl_Position        = projectionMatrix * viewMatrix * worldPosition;
 	gl_ClipDistance[0] = dot(worldPosition, clipPlane);
 	txCoords           = textureCoords;
 
 	vec4 transNormal = instances[gl_InstanceID].modelMatrix * vec4(normal, 0.0f);
-	vec3 unitNormal  = normalize(transNormal.xyz);
+	unitNormal       = normalize(transNormal.xyz);
 
-	for (int i = 0; i < min(MAX_LIGHTS, NUM_LIGHTS); ++i)
+	for (int i = 0; i < MAX_LIGHTS; ++i)
 	{
-		vec3  lightDir  = normalize(lights[i].position.xyz - worldPosition.xyz);
-		float attFactor = CalculateAttFactor(i, worldPosition);
-		vec4  ambient   = CalculateAmbient(i) * attFactor;
-		vec4  diffuse   = CalculateDiffuse(i, unitNormal, lightDir);
-		vertexColor    += ambient + diffuse;
+		unitLightVector[i] = normalize(lights[i].position.xyz - worldPosition.xyz);
 	}
-}
-
-float CalculateAttFactor(int index, vec4 worldPosition)
-{
-	vec4  ATT       = lights[index].attenuation;
-	float distance  = length(lights[index].position.xyz - worldPosition.xyz);
-	float attFactor = ATT.x + (ATT.y * distance) + (ATT.z * distance * distance);
-	return 1.0f / attFactor;
-}
-
-vec4 CalculateAmbient(int index)
-{
-	return vec4(AMBIENT_STRENGTH * lights[index].ambient.rgb, 1.0f);
-}
-
-vec4 CalculateDiffuse(int index, vec3 normal, vec3 lightDir)
-{
-	float nDot1      = dot(normal, lightDir);
-	float brightness = max(nDot1, 0.0f);
-	return vec4(brightness * lights[index].diffuse.rgb, 1.0f);
 }
