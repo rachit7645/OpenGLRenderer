cce72598ec3e598ee396bdcf6b8b3c01e7dd9520
Rachit Khandelwal <rachit7645@gmail.com>
2023-04-02 17:36:36 +0530

    Update dependencies

--- ImGui.h
+++ ImGui.h
@@ -1,8 +1,14 @@
 #ifndef RACHIT_IMGUI_H
 #define RACHIT_IMGUI_H
 
+// TODO: Properly fix ImGui Internal header
+
+// Fixes an error with imgui_internal.h
+#define IMGUI_DEFINE_MATH_OPERATORS
+
+#include "imgui/imgui_internal.h"
 #include "imgui/imgui.h"
-#include "imgui/backends/imgui_impl_sdl.h"
+#include "imgui/backends/imgui_impl_sdl2.h"
 #include "imgui/backends/imgui_impl_opengl3.h"
 #include "imgui/misc/cpp/imgui_stdlib.h"
 
